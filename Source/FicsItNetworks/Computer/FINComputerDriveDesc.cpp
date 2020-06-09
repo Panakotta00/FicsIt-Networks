668b60c03c99c5d40b58e9f2ef28bbdadc65287f
Panakotta00 <panakotta00@gmail.com>
2020-06-10 00:17:01 +0200

    Drive Tooltip

--- Source/FicsItNetworks/Computer/FINComputerDriveDesc.cpp
+++ Source/FicsItNetworks/Computer/FINComputerDriveDesc.cpp
@@ -1,7 +1,51 @@
 #include "FINComputerDriveDesc.h"
 
+#include "HorizontalBox.h"
+#include "HorizontalBoxSlot.h"
+#include "TextBlock.h"
+#include "FicsItKernel/FicsItFS/FINFileSystemState.h"
+
 UFINComputerDriveDesc::UFINComputerDriveDesc() {
 	mStackSize = EStackSize::SS_ONE;
+}
+
+FText UFINComputerDriveDesc::GetOverridenItemName_Implementation(APlayerController* OwningPlayer, const FInventoryStack& InventoryStack) {
+	return UFGItemDescriptor::GetItemName(InventoryStack.Item.ItemClass);
+}
+
+FText UFINComputerDriveDesc::GetOverridenItemDescription_Implementation(APlayerController* OwningPlayer, const FInventoryStack& InventoryStack) {
+	return FText();
+}
+
+UWidget* UFINComputerDriveDesc::CreateDescriptionWidget_Implementation(APlayerController* OwningPlayer, const FInventoryStack& InventoryStack) {
+	UClass* progressBar = LoadObject<UClass>(NULL, TEXT("/Game/FactoryGame/Interface/UI/InGame/-Shared/Widget_ProgressBar.Widget_ProgressBar_C"));
+	
+	AFINFileSystemState* state = Cast<AFINFileSystemState>(InventoryStack.Item.ItemState.Get());
+	if (!IsValid(state)) return nullptr;
+
+	FileSystem::SRef<FileSystem::ByteCountedDevice> byteCounted = state->GetDevice();
+	float usage = 0.0;
+	if (byteCounted.isValid()) usage = static_cast<float>(byteCounted->getSize()) / static_cast<float>(byteCounted->capacity);
+	
+	UHorizontalBox* horizontal = NewObject<UHorizontalBox>(OwningPlayer);
+	UTextBlock* prefix = NewObject<UTextBlock>(OwningPlayer);
+	prefix->Font.Size = 8;
+	prefix->SetText(FText::FromString("Usage:"));
+	horizontal->AddChild(prefix);
+	Cast<UHorizontalBoxSlot>(prefix->Slot)->SetVerticalAlignment(EVerticalAlignment::VAlign_Center);
+	UUserWidget* progress = CreateWidget(OwningPlayer, progressBar);
+	struct {
+		float start, end, time;
+	} params = {0.0, usage, 0.1};
+	progress->ProcessEvent(progress->FindFunction("LerpBar"), &params);
+	FLinearColor& color = *Cast<UStructProperty>(progressBar->FindPropertyByName("mBarTint"))->ContainerPtrToValuePtr<FLinearColor>(progress);
+	color = FLinearColor(1,1,1);
+	horizontal->AddChild(progress);
+	UHorizontalBoxSlot* progressSlot = Cast<UHorizontalBoxSlot>(progress->Slot);
+	progressSlot->SetVerticalAlignment(EVerticalAlignment::VAlign_Center);
+	progressSlot->SetHorizontalAlignment(EHorizontalAlignment::HAlign_Fill);
+	progressSlot->SetSize(FSlateChildSize(ESlateSizeRule::Fill));
+	return horizontal;
 }
 
 int32 UFINComputerDriveDesc::GetStorageCapacity(TSubclassOf<UFINComputerDriveDesc> drive) {
--- Source/FicsItNetworks/Computer/FINComputerDriveDesc.h
+++ Source/FicsItNetworks/Computer/FINComputerDriveDesc.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "FGItemDescriptor.h"
+#include "tooltip/ItemTooltipHandler.h"
+
 #include "FINComputerDriveDesc.generated.h"
 
 UCLASS()
-class UFINComputerDriveDesc : public UFGItemDescriptor {
+class UFINComputerDriveDesc : public UFGItemDescriptor, public ISMLItemDisplayInterface {
 	GENERATED_BODY()
 
 public:
@@ -16,9 +18,15 @@ public:
 
 	UFINComputerDriveDesc();
 
-	UFUNCTION(BlueprintCallable, Category = "FileSystem|Drive")
-		static int32 GetStorageCapacity(TSubclassOf<UFINComputerDriveDesc> drive);
+	// Begin ISMLItemDisplayInterface
+	virtual FText GetOverridenItemName_Implementation(APlayerController* OwningPlayer, const FInventoryStack& InventoryStack) override;
+	virtual FText GetOverridenItemDescription_Implementation(APlayerController* OwningPlayer, const FInventoryStack& InventoryStack) override;
+	virtual UWidget* CreateDescriptionWidget_Implementation(APlayerController* OwningPlayer, const FInventoryStack& InventoryStack) override;
+	// End ISMLItemDisplayInterface
 
 	UFUNCTION(BlueprintCallable, Category = "FileSystem|Drive")
-		static UTexture2D* GetDriveInventoryImage(TSubclassOf<UFINComputerDriveDesc> drive);
+	static int32 GetStorageCapacity(TSubclassOf<UFINComputerDriveDesc> drive);
+
+	UFUNCTION(BlueprintCallable, Category = "FileSystem|Drive")
+	static UTexture2D* GetDriveInventoryImage(TSubclassOf<UFINComputerDriveDesc> drive);
 };
