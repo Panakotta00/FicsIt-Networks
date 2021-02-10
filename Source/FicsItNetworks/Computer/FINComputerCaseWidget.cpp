402b93346a7e066d8aca84433500034ef07d0d02
Panakotta00 <panakotta00@gmail.com>
2021-02-10 15:56:04 +0100

    Make computer case widget depend on CPU

---
+++ Source/FicsItNetworks/Computer/FINComputerCaseWidget.cpp
@@ -0,0 +1,1 @@
+﻿#include "FINComputerCaseWidget.h"
---
+++ Source/FicsItNetworks/Computer/FINComputerCaseWidget.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "UserWidget.h"
+#include "FicsItKernel/Processor/FINStateEEPROM.h"
+
+#include "FINComputerCaseWidget.generated.h"
+
+class AFINComputerCase;
+
+UCLASS(Blueprintable)
+class UFINComputerCaseWidget : public UUserWidget {
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ExposeOnSpawn="true"))
+	AFINComputerCase* Computer = nullptr;
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void OnEEPROMUpdate(AFINStateEEPROM* EEPROM);
+};
