c226020a7b180356bafebc857d6c3f354e5a1834
Panakotta00 <panakotta00@gmail.com>
2021-05-29 02:00:02 +0200

    InternetCard more solid request data

--- Source/FicsItNetworks/Computer/FINInternetCard.cpp
+++ Source/FicsItNetworks/Computer/FINInternetCard.cpp
@@ -12,18 +12,27 @@ void FFINInternetCardHttpRequestFuture::Execute() {
 }
 
 bool FFINInternetCardHttpRequestFuture::IsDone() const {
-	return Request.IsValid() && !(Request->GetStatus() == EHttpRequestStatus::Processing || Request->GetStatus() == EHttpRequestStatus::NotStarted);
+	return !Request.IsValid() || !(Request->GetStatus() == EHttpRequestStatus::Processing || Request->GetStatus() == EHttpRequestStatus::NotStarted);
 }
 
 TArray<FFINAnyNetworkValue> FFINInternetCardHttpRequestFuture::GetOutput() const {
 	TArray<FFINAnyNetworkValue> Response;
 	if (!Request.IsValid() || !Request->GetResponse().IsValid()) return Response;
+	Response.Add((FINInt)Request->GetResponse()->GetResponseCode());
 	Response.Add(Request->GetResponse()->GetContentAsString());
+	TArray<FINAny> Headers;
+	for (FString Header : Request->GetResponse()->GetAllHeaders()) {
+		FString Name;
+		FString Value;
+		Header.Split(": ", &Name, &Value);
+		Headers.Add(Name);
+		Headers.Add(Value);
+	}
+	Response.Add(Headers);
 	return Response;
 }
 
-
-FFINInternetCardHttpRequestFuture AFINInternetCard::netFunc_request(const FString& InURL, const FString& InMethod, const FString& InData, const TArray<FFINAnyNetworkValue>& varargs) {
+FFINInternetCardHttpRequestFuture AFINInternetCard::netFunc_request(const FString& InURL, const FString& InMethod, const FString& InData, TArray<FFINAnyNetworkValue> varargs) {
 	TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
 	Request->SetURL(InURL);
 	Request->SetVerb(InMethod);
@@ -32,6 +41,9 @@ FFINInternetCardHttpRequestFuture AFINInternetCard::netFunc_request(const FStrin
 		FString Name = varargs[i].GetString();
 		FString Value = varargs[i+1].GetString();
 		Request->SetHeader(Name, Value);
+	}
+	if (!InData.IsEmpty() && Request->GetHeader(L"Content-Type").IsEmpty()) {
+		throw FFINException("Req-Payload given without Content-Type");
 	}
 	return FFINInternetCardHttpRequestFuture(Request);
 }
--- Source/FicsItNetworks/Computer/FINInternetCard.h
+++ Source/FicsItNetworks/Computer/FINInternetCard.h
@@ -8,20 +8,6 @@
 #include "FINInternetCard.generated.h"
 
 USTRUCT()
-struct FFINInternetCardHttpResponse {
-	GENERATED_BODY()
-	
-	UPROPERTY(SaveGame)
-	FString Content;
-
-	UPROPERTY(SaveGame)
-	int StatusCode;
-
-	UPROPERTY(SaveGame)
-	TArray<FString> Headers;
-};
-
-USTRUCT()
 struct FFINInternetCardHttpRequestFuture : public FFINFuture {
 	GENERATED_BODY()
 private:
@@ -43,5 +29,21 @@ class AFINInternetCard : public AFINComputerModule, public IFINPciDeviceInterfac
 	GENERATED_BODY()
 public:
 	UFUNCTION()
-	FFINInternetCardHttpRequestFuture netFunc_request(const FString& InURL, const FString& InMethod, const FString& InData, const TArray<FFINAnyNetworkValue>& varargs);
+	FFINInternetCardHttpRequestFuture netFunc_request(const FString& InURL, const FString& InMethod, const FString& InData, TArray<FFINAnyNetworkValue> varargs);
+	UFUNCTION()
+	void netFuncMeta_send(FString& InternalName, FText& DisplayName, FText& Description, TArray<FString>& ParameterInternalNames, TArray<FText>& ParameterDisplayNames, TArray<FText>& ParameterDescriptions, int32& Runtime) {
+		InternalName = "request";
+		DisplayName = FText::FromString("Request");
+		Description = FText::FromString("Does an HTTP-Request. If a payload is given, the Content-Type header has to be set. All additional parameters have to be strings and in pairs of to for defining the http headers and values.");
+		ParameterInternalNames.Add("url");
+		ParameterDisplayNames.Add(FText::FromString("URL"));
+		ParameterDescriptions.Add(FText::FromString("The URL for which you want to make an HTTP Request."));
+		ParameterInternalNames.Add("method");
+		ParameterDisplayNames.Add(FText::FromString("Method"));
+		ParameterDescriptions.Add(FText::FromString("The http request method/verb you want to make the request. f.e. 'GET', 'POST'"));
+		ParameterInternalNames.Add("data");
+		ParameterDisplayNames.Add(FText::FromString("Data"));
+		ParameterDescriptions.Add(FText::FromString("The http request payload you want to sent."));
+		Runtime = 1;
+	}
 };
