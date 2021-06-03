23fc48aff5df8cd122b6bcb9a23d8328315df3bb
Panakotta00 <panakotta00@gmail.com>
2021-06-04 01:44:19 +0200

    Change to FiraCode-Font for code editor

--- Source/FicsItNetworks/UI/FINLuaCodeEditor.cpp
+++ Source/FicsItNetworks/UI/FINLuaCodeEditor.cpp
@@ -324,6 +324,7 @@ void SFINLuaCodeEditor::Construct(const FArguments& InArgs) {
 		.Style(InArgs._Style)
 		.OnTextChanged(InArgs._OnTextChanged)
 		.OnTextCommitted(InArgs._OnTextCommitted)
+		.TextShapingMethod(ETextShapingMethod::FullShaping)
 	];
 }
 
