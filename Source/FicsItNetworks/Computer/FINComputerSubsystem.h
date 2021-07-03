0ccd7a368c70ff62607912129c396c6820460354
Patrick Dobbs <ptsd@patrickdobbs.co.uk>
2021-07-03 07:42:47 +0100

    Make WidgetInteraction a dependency.
    Remove all WidgetInteraction related stuff.
    
    Add PTSDInteraction as a dependency.

--- Source/FicsItNetworks/Computer/FINComputerSubsystem.h
+++ Source/FicsItNetworks/Computer/FINComputerSubsystem.h
@@ -14,41 +14,18 @@ class FICSITNETWORKS_API AFINComputerSubsystem : public AModSubsystem, public IF
 	GENERATED_BODY()
 	
 public:
-	UPROPERTY()
-	TMap<AFGCharacterPlayer*, UWidgetInteractionComponent*> ScreenInteraction;
-
-	UPROPERTY()
-	UInputComponent* Input;
-
 	UPROPERTY(SaveGame, BlueprintReadOnly)
 	TEnumAsByte<EFINCustomVersion> Version = EFINCustomVersion::FINBeforeCustomVersionWasAdded;
 
 	int VirtualUserNum = 0;
 
 	AFINComputerSubsystem();
-
-	// Begin AActor
-	virtual void OnConstruction(const FTransform& Transform) override;
-	virtual void BeginPlay() override;
-	virtual void Tick(float dt) override;
-	// End AActor
 	
 	// Begin IFGSaveInterface
 	virtual bool ShouldSave_Implementation() const override;
 	virtual void PreSaveGame_Implementation(int32 saveVersion, int32 gameVersion) override;
 	// End IFGSaveInterface
 
-	void OnPrimaryFirePressed();
-	void OnPrimaryFireReleased();
-	void OnSecondaryFirePressed();
-	void OnSecondaryFireReleased();
-
 	UFUNCTION(BlueprintCallable, Category = "Computer", meta = (WorldContext = "WorldContext"))
 	static AFINComputerSubsystem* GetComputerSubsystem(UObject* WorldContext);
-
-	UFUNCTION(BlueprintCallable, Category = "Computer")
-	void AttachWidgetInteractionToPlayer(AFGCharacterPlayer* character);
-	
-	UFUNCTION(BlueprintCallable, Category = "Computer")
-	void DetachWidgetInteractionToPlayer(AFGCharacterPlayer* character);
 };
--- Source/FicsItNetworks/FicsItNetworksModule.cpp
+++ Source/FicsItNetworks/FicsItNetworksModule.cpp
@@ -139,22 +139,6 @@ void FFicsItNetworksModule::StartupModule(){
 		SUBSCRIBE_METHOD_VIRTUAL(IFGDismantleInterface::Dismantle_Implementation, (void*)static_cast<const IFGDismantleInterface*>(GetDefault<AFGBuildable>()), &AFGBuildable_Dismantle_Implementation)
 		SUBSCRIBE_METHOD_VIRTUAL(IFGDismantleInterface::GetDismantleRefund_Implementation, (void*)static_cast<const IFGDismantleInterface*>(GetDefault<AFGBuildable>()), &AFGBuildable_GetDismantleRefund_Implementation)
 		
-		SUBSCRIBE_METHOD_VIRTUAL_AFTER(AFGCharacterPlayer::BeginPlay, (void*)GetDefault<AFGCharacterPlayer>(), [](AActor* self) {
-			AFGCharacterPlayer* character = Cast<AFGCharacterPlayer>(self);
-			if (character) {
-				AFINComputerSubsystem* SubSys = AFINComputerSubsystem::GetComputerSubsystem(self->GetWorld());
-				if (SubSys) SubSys->AttachWidgetInteractionToPlayer(character);
-			}
-		})
-
-		SUBSCRIBE_METHOD_VIRTUAL_AFTER(AFGCharacterPlayer::EndPlay, (void*)GetDefault<AFGCharacterPlayer>(), [](AActor* self, EEndPlayReason::Type Reason) {
-			AFGCharacterPlayer* character = Cast<AFGCharacterPlayer>(self);
-			if (character) {
-				AFINComputerSubsystem* SubSys = AFINComputerSubsystem::GetComputerSubsystem(self->GetWorld());
-				if (SubSys) SubSys->DetachWidgetInteractionToPlayer(character);
-			}
-		})
-
 		SUBSCRIBE_METHOD_VIRTUAL_AFTER(AFGGameMode::PostLogin, (void*)GetDefault<AFGGameMode>(), [](AFGGameMode* gm, APlayerController* pc) {
 			if (gm->HasAuthority() && !gm->IsMainMenuGameMode()) {
 				gm->RegisterRemoteCallObjectClass(UFINComputerRCO::StaticClass());
