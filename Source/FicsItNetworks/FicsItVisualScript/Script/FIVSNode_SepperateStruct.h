7d887d1a51eeff1c1e443ca236945d750b1d6530
Panakotta00 <panakotta00@gmail.com>
2021-10-22 08:55:22 +0200

    FIVS Break/Make Struct (aka. Struct support)

---
+++ Source/FicsItNetworks/FicsItVisualScript/Script/FIVSNode_SepperateStruct.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "FIVSScriptNode.h"
+#include "FIVSNode_SepperateStruct.generated.h"
+
+UCLASS()
+class UFIVSNode_SepperateStruct : public UFIVSScriptNode {
+	GENERATED_BODY()
+
+	UPROPERTY()
+	UFINStruct* Struct = nullptr;
+	UPROPERTY()
+	bool bBreak = true;
+
+	UPROPERTY()
+	TMap<FString, UFIVSPin*> InputPins;
+	
+public:
+	// Begin UFIVSNode
+	virtual void InitPins() override;
+	virtual TArray<FFIVSNodeAction> GetNodeActions() const override;
+	virtual void SerializeNodeProperties(FFIVSNodeProperties& Properties) const override;
+	virtual void DeserializeNodeProperties(const FFIVSNodeProperties& Properties) override;
+	virtual FString GetNodeName() const override;
+	// End UFIVSNode
+
+	// Begin UFIVSScriptNode
+	virtual TArray<UFIVSPin*> PreExecPin(UFIVSPin* ExecPin, FFIVSRuntimeContext& Context) override;
+	virtual UFIVSPin* ExecPin(UFIVSPin* ExecPin, FFIVSRuntimeContext& Context) override;
+	// End UFIVSScriptNode
+};
