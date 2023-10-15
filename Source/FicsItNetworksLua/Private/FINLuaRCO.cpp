533ddf18763ecc0e60b61c1070e8b72edccc2b84
Panakotta00 <panakotta00@gmail.com>
2023-10-15 16:56:38 +0200

    Fix some Lua UI & Log Viewer issues

--- Source/FicsItNetworksLua/Private/FINLuaRCO.cpp
+++ Source/FicsItNetworksLua/Private/FINLuaRCO.cpp
@@ -1,5 +1,7 @@
 #include "FINLuaRCO.h"
 
+#include "FINStateEEPROMLua.h"
+
 void UFINLuaRCO::SetLuaEEPROMCode_Implementation(AFINStateEEPROMLua* LuaEEPROMState, const FString& NewCode) {
 	LuaEEPROMState->SetCode(NewCode);
 }
