399ca5c9a11ba859d427fe43aa34027778800096
Panakotta00 <panakotta00@gmail.com>
2024-07-24 19:47:05 +0200

    feat: Corrections for GenLuaDoc & Operator support

--- Source/FicsItNetworksLua/Private/CMD/FINCMDGenLuaDoc.cpp
+++ Source/FicsItNetworksLua/Private/CMD/FINCMDGenLuaDoc.cpp
@@ -53,6 +53,59 @@ namespace FINGenLuaDoc {
 		}
 	}
 
+	TOptional<FString> MatchLuaOperator(UFINFunction* Func) {
+		TOptional<EFINOperator> op = Func->IsOperator();
+		if (!op.IsSet()) return {};
+		switch (*op) {
+			case FIN_Op_Add:
+				return FString(TEXT("add"));
+			case FIN_Op_Sub:
+				return FString(TEXT("sub"));
+			case FIN_Op_Mul:
+				return FString(TEXT("mul"));
+			case FIN_Op_Div:
+				return FString(TEXT("div"));
+			case FIN_Op_Mod:
+				return FString(TEXT("mod"));
+			case FIN_Op_Pow:
+				return FString(TEXT("pow"));
+			case FIN_Op_Neg:
+				return FString(TEXT("unm"));
+			case FIN_Op_FDiv:
+				return FString(TEXT("idiv"));
+			case FIN_Op_BitAND:
+				return FString(TEXT("band"));
+			case FIN_Op_BitOR:
+				return FString(TEXT("bor"));
+			case FIN_Op_BitXOR:
+				return FString(TEXT("bxor"));
+			case FIN_Op_BitNOT:
+				return FString(TEXT("bnot"));
+			case FIN_Op_ShiftL:
+				return FString(TEXT("shl"));
+			case FIN_Op_ShiftR:
+				return FString(TEXT("shr"));
+			case FIN_Op_Concat:
+				return FString(TEXT("concat"));
+			case FIN_Op_Len:
+				return FString(TEXT("len"));
+			case FIN_Op_Equals:
+				return FString(TEXT("eq"));
+			case FIN_Op_LessThan:
+				return FString(TEXT("lt"));
+			case FIN_Op_LessOrEqualThan:
+				return FString(TEXT("le"));
+			case FIN_Op_Index:
+				return FString(TEXT("index"));
+			case FIN_Op_NewIndex:
+				return FString(TEXT("newindex"));
+			case FIN_Op_Call:
+				return FString(TEXT("call"));
+			default:
+				return {};
+		}
+	}
+
 	FString GetInlineDescription(FString Description) {
 		Description.ReplaceInline(TEXT("\r\n"), TEXT("\n"));
 
@@ -76,7 +129,9 @@ namespace FINGenLuaDoc {
 		Documentation.Appendf(TEXT("---@field public %s %s %s\n"), *Prop->GetInternalName(), *GetType(Ref, Prop), *GetInlineDescription(Prop->GetDescription().ToString()));
 	}
 
-	void WriteFunction(FStringBuilderBase& Str, FFINReflection& Ref, FString Parent, UFINFunction* Func) {
+	void WriteFunction(FStringBuilderBase& Str, FFINReflection& Ref, FString Parent, FString Type, UFINFunction* Func) {
+		if (MatchLuaOperator(Func)) return;
+
 		WriteMultiLineDescription(Str, Func->GetDescription().ToString());
 
 		TArray<UFINProperty*> parameters;
@@ -91,25 +146,50 @@ namespace FINGenLuaDoc {
 			}
 		}
 
-		FString ParamList;
+		TArray<FString> paramList;
+		TArray<FString> typedParameterList;
+		typedParameterList.Add(TEXT("self:") + Type);
 		for (UFINProperty* parameter : parameters) {
-			if (ParamList.Len() > 0) ParamList.Append(", ");
-			ParamList.Append(parameter->GetInternalName());
+			paramList.Add(parameter->GetInternalName());
+			typedParameterList.Add(parameter->GetInternalName() + TEXT(":") + GetType(Ref, parameter));
 			Str.Appendf(TEXT("---@param %s %s %s\n"), *parameter->GetInternalName(), *GetType(Ref, parameter), *GetInlineDescription(parameter->GetDescription().ToString()));
 		}
+		if (Func->FunctionFlags & FIN_Func_VarArgs) {
+			paramList.Add(TEXT("..."));
+			typedParameterList.Add(TEXT("...:any"));
+			Str.Append(TEXT("---@param ... any\n"));
+		}
+
+		TArray<FString> returnValueList;
 		for (UFINProperty* returnValue : returnValues) {
+			returnValueList.Add(returnValue->GetInternalName() + TEXT(":") + GetType(Ref, returnValue));
 			Str.Appendf(TEXT("---@return %s %s %s\n"), *GetType(Ref, returnValue), *returnValue->GetInternalName(), *GetInlineDescription(returnValue->GetDescription().ToString()));
 		}
-		Str.Appendf(TEXT("function %s:%s(%s) end\n"), *Parent, *Func->GetInternalName(), *ParamList);
+		if (Func->FunctionFlags & FIN_Func_VarRets) {
+			returnValueList.Add(TEXT("...:any"));
+			Str.Append(TEXT("---@return ... any\n"));
+		}
+
+		FString functionTypedReturn;
+		if (!returnValueList.IsEmpty()) {
+			functionTypedReturn = FString::Printf(TEXT(":(%s)"), *FString::Join(returnValueList, TEXT(",")));
+		}
+
+		Str.Appendf(TEXT("---@type (fun(%s)%s)|ReflectionFunction\n"), *FString::Join(typedParameterList, TEXT(",")), *functionTypedReturn);
+
+		Str.Appendf(TEXT("function %s:%s(%s) end\n"), *Parent, *Func->GetInternalName(), *FString::Join(paramList, TEXT(", ")));
 
 		// TODO: Add support for Operator Overloading
 	}
 
 	void WriteStruct(FStringBuilderBase& Str, FFINReflection& Ref, UFINStruct* Struct) {
+		if (Struct->GetInternalName() == TEXT("Future")) return;
+
 		WriteMultiLineDescription(Str,  Struct->GetDescription().ToString());
 
 		{
-			FString ClassDeclaration = Struct->GetInternalName();
+			FString ClassIdentifier = Struct->GetInternalName();
+			FString ClassDeclaration = ClassIdentifier;
 			if (UFINStruct* parent = Struct->GetParent()) {
 				ClassDeclaration += TEXT(" : ") + Struct->GetParent()->GetInternalName();
 			}
@@ -121,11 +201,30 @@ namespace FINGenLuaDoc {
 				}
 			}
 
+			for (UFINFunction* Func : Struct->GetFunctions(false)) {
+				if (!((Func->GetFunctionFlags() & FIN_Func_MemberFunc) && !(Func->GetFunctionFlags() & FIN_Func_ClassFunc))) continue;
+				TOptional<FString> op = MatchLuaOperator(Func);
+				if (!op) continue;
+				FString parameter;
+				FString returnValue;
+				for (UFINProperty* prop : Func->GetParameters()) {
+					if (prop->GetPropertyFlags() & FIN_Prop_Param){
+						if (prop->GetPropertyFlags() & FIN_Prop_OutParam) {
+							returnValue = GetType(Ref, prop);
+						} else {
+							parameter = GetType(Ref, prop);
+						}
+					}
+				}
+
+				Str.Appendf(TEXT("---@operator %s(%s):%s\n"), **op, *parameter, *returnValue);
+			}
+
 			Str.Appendf(TEXT("%s = {}\n"), *Struct->GetInternalName());
 
 			for (UFINFunction* Func : Struct->GetFunctions(false)) {
 				if ((Func->GetFunctionFlags() & FIN_Func_MemberFunc) && !(Func->GetFunctionFlags() & FIN_Func_ClassFunc)) {
-					WriteFunction(Str, Ref, Struct->GetInternalName(), Func);
+					WriteFunction(Str, Ref, Struct->GetInternalName(), ClassIdentifier, Func);
 				}
 			}
 		}
@@ -133,9 +232,10 @@ namespace FINGenLuaDoc {
 		{
 			WriteMultiLineDescription(Str, Struct->GetDescription().ToString());
 
-			FString ClassDeclaration = Struct->GetInternalName();
+			FString ClassIdentifier = Struct->GetInternalName() + TEXT("-Class");
+			FString ClassDeclaration = ClassIdentifier;
 			if (UFINStruct* parent = Struct->GetParent()) {
-				ClassDeclaration += TEXT("-Class : ") + Struct->GetParent()->GetInternalName() + TEXT("-Class");
+				ClassDeclaration += TEXT(" : ") + Struct->GetParent()->GetInternalName() + TEXT("-Class");
 			}
 			Str.Appendf(TEXT("---@class %s\n"), *ClassDeclaration);
 
@@ -149,7 +249,7 @@ namespace FINGenLuaDoc {
 
 			for (UFINFunction* Func : Struct->GetFunctions(false)) {
 				if ((Func->GetFunctionFlags() & FIN_Func_MemberFunc) && (Func->GetFunctionFlags() & FIN_Func_ClassFunc)) {
-					WriteFunction(Str, Ref, Struct->GetInternalName(), Func);
+					WriteFunction(Str, Ref, Struct->GetInternalName(), ClassIdentifier, Func);
 				}
 			}
 		}
@@ -171,18 +271,27 @@ namespace FINGenLuaDoc {
 	}
 
 	void WriteLuaFunction(FStringBuilderBase& Str, const FFINLuaFunction& Function, const FString& Identifier) {
-		FString parameterList;
+		TArray<FString> parameterList;
+		TArray<FString> typedParameterList;
 		for (const auto& parameter : Function.Parameters) {
 			Str.Appendf(TEXT("---@param %s %s %s\n"), *parameter.InternalName, *parameter.Type, *parameter.Description.ToString());
-			if (parameterList.Len() > 0) {
-				parameterList.Append(TEXT(", "));
-			}
-			parameterList.Append(parameter.InternalName);
+			parameterList.Add(parameter.InternalName);
+			typedParameterList.Add(parameter.InternalName + TEXT(":") + parameter.Type);
 		}
+
+		TArray<FString> typedReturnValueList;
 		for (const auto& returnValue : Function.ReturnValues) {
 			Str.Appendf(TEXT("---@return %s %s %s\n"), *returnValue.Type, *returnValue.InternalName, *returnValue.Description.ToString());
+			typedReturnValueList.Add(returnValue.InternalName + TEXT(":") + returnValue.Type);
 		}
-		Str.Appendf(TEXT("function %s(%s) end\n"), *Identifier, *parameterList);
+
+		FString functionReturn;
+		if (!typedReturnValueList.IsEmpty()) {
+			functionReturn = FString::Printf(TEXT(":(%s)"), *FString::Join(typedReturnValueList, TEXT(",")));
+		}
+
+		Str.Appendf(TEXT("---@type (fun(%s)%s):ModuleTableFunction\n"), *FString::Join(typedParameterList, TEXT(",")), *functionReturn);
+		Str.Appendf(TEXT("function %s(%s) end\n"), *Identifier, *FString::Join(parameterList, TEXT(", ")));
 	}
 
 	void WriteLuaValue(FStringBuilderBase& Str, const TSharedPtr<FFINLuaModuleValue>& Value, const FString& Identifier) {
@@ -250,6 +359,11 @@ namespace FINGenLuaDoc {
 
 			str.Append(TEXT("---@meta\n\n"));
 
+			auto& moduleRegistry = FFINLuaModuleRegistry::GetInstance();
+			for (const TSharedRef<FFINLuaModule>& module : moduleRegistry.Modules) {
+				WriteModule(str, module);
+			}
+
 			FFINReflection& reflection = *FFINReflection::Get();
 			for (TPair<UClass*, UFINClass*> Class : reflection.GetClasses()) {
 				WriteStruct(str, reflection, Class.Value);
@@ -258,15 +372,13 @@ namespace FINGenLuaDoc {
 				WriteStruct(str, reflection, Struct.Value);
 			}
 
-			auto& moduleRegistry = FFINLuaModuleRegistry::GetInstance();
-			for (const TSharedRef<FFINLuaModule>& module : moduleRegistry.Modules) {
-				WriteModule(str, module);
-			}
-
 			UE_LOG(LogFicsItNetworksLua, Display, TEXT("FicsIt-Networks Lua Documentation generated!"));
 
-			FString Path = FPaths::Combine(FPlatformProcess::UserSettingsDir(), FApp::GetProjectName(), TEXT("Saved/"));
-			Path = FPaths::Combine(Path, TEXT("FINLuaDocumentation.lua"));
+			FString Path;
+			if (!FParse::Value(Command, TEXT("Path"), Path)) {
+				Path = FPaths::Combine(FPlatformProcess::UserSettingsDir(), FApp::GetProjectName(), TEXT("Saved/"));
+				Path = FPaths::Combine(Path, TEXT("FINLuaDocumentation.lua"));
+			}
 
 			UE_LOGFMT(LogFicsItNetworksLua, Display, "Saving FicsIt-Networks Lua Documentation under: {Path}", Path);
 
