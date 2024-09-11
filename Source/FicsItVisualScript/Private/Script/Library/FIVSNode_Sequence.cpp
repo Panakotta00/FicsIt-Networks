a35fd69c80d89c7a6cdbc942b4f95156db67e4da
Panakotta00 <panakotta00@gmail.com>
2024-09-11 21:18:53 +0200

    feat: FIVS add sequence node

---
+++ Source/FicsItVisualScript/Private/Script/Library/FIVSNode_Sequence.cpp
@@ -0,0 +1,63 @@
+﻿#include "Script/Library/FIVSNode_Sequence.h"
+
+#include "Editor/FIVSEdNodeViewer.h"
+
+UFIVSNode_Sequence::UFIVSNode_Sequence() {
+	DisplayName = FText::FromString(TEXT("Sequence"));
+	ExecIn = CreateDefaultPin(FIVS_PIN_EXEC_INPUT, TEXT("Exec"), FText::FromString(TEXT("Exec")));
+	ExecOut.Add(CreateDefaultPin(FIVS_PIN_EXEC_OUTPUT, TEXT("Then1"), FText::FromString(TEXT("Then 1"))));
+	ExecOut.Add(CreateDefaultPin(FIVS_PIN_EXEC_OUTPUT, TEXT("Then2"), FText::FromString(TEXT("Then 2"))));
+}
+
+void UFIVSNode_Sequence::GetNodeActions(TArray<FFIVSNodeAction>& Actions) const {
+	FFIVSNodeAction action = FFIVSNodeAction{
+		UFIVSNode_Sequence::StaticClass(),
+		FText::FromString(TEXT("Sequence")),
+		FText::FromString(TEXT("General")),
+		FText::FromString(TEXT("Sequence")),
+		{
+			FIVS_PIN_EXEC_INPUT,
+			FIVS_PIN_EXEC_OUTPUT,
+		},
+	};
+	Actions.Add(action);
+}
+
+TSharedRef<SFIVSEdNodeViewer> UFIVSNode_Sequence::CreateNodeViewer(const TSharedRef<SFIVSEdGraphViewer>& GraphViewer, const FFIVSEdNodeStyle* Style) {
+	return SNew(SFIVSEdFunctionNodeViewer, GraphViewer, this)
+		.Style(Style)
+		.Footer()[
+			SNew(SHorizontalBox)
+			+SHorizontalBox::Slot().HAlign(HAlign_Right)[
+				SNew(SButton)
+				.Text(FText::FromString(TEXT("Add Pin")))
+				.OnClicked_Lambda([this]() {
+					SetOutputNum(ExecOut.Num() + 1);
+
+					return FReply::Handled();
+				})
+			]
+		];
+}
+
+TArray<UFIVSPin*> UFIVSNode_Sequence::ExecPin(UFIVSPin* ExecPin, FFIVSRuntimeContext& Context) {
+	return ExecOut;
+}
+
+void UFIVSNode_Sequence::SerializeNodeProperties(FFIVSNodeProperties& Properties) const {
+	Super::SerializeNodeProperties(Properties);
+}
+
+void UFIVSNode_Sequence::DeserializeNodeProperties(const FFIVSNodeProperties& Properties) {
+	Super::DeserializeNodeProperties(Properties);
+}
+
+void UFIVSNode_Sequence::SetOutputNum(int32 OutputNum) {
+	while (ExecOut.Num() > OutputNum) {
+		DeletePin(ExecOut.Pop());
+	}
+	while (ExecOut.Num() < OutputNum) {
+		int32 i = ExecOut.Num() + 1;
+		ExecOut.Add(CreatePin(FIVS_PIN_EXEC_OUTPUT, FString::Printf(TEXT("Then%i"), i), FText::FromString(FString::Printf(TEXT("Then %i"), i))));
+	}
+}
---
+++ Source/FicsItVisualScript/Public/Script/Library/FIVSNode_Sequence.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Script/FIVSScriptNode.h"
+#include "FIVSNode_Sequence.generated.h"
+
+UCLASS()
+class UFIVSNode_Sequence : public UFIVSScriptNode {
+	GENERATED_BODY()
+private:
+	UPROPERTY()
+	UFIVSPin* ExecIn = nullptr;
+	UPROPERTY()
+	TArray<UFIVSPin*> ExecOut;
+
+public:
+	UFIVSNode_Sequence();
+
+	// Begin UFIVSNode
+	virtual void GetNodeActions(TArray<FFIVSNodeAction>& Actions) const override;
+	// End UFIVSNode
+
+	// Begin UFIVSScriptNode
+	virtual TSharedRef<SFIVSEdNodeViewer> CreateNodeViewer(const TSharedRef<SFIVSEdGraphViewer>& GraphViewer, const FFIVSEdNodeStyle* Style) override;
+	virtual TArray<UFIVSPin*> ExecPin(UFIVSPin* ExecPin, FFIVSRuntimeContext& Context) override;
+	virtual void SerializeNodeProperties(FFIVSNodeProperties& Properties) const override;
+	virtual void DeserializeNodeProperties(const FFIVSNodeProperties& Properties) override;
+	// End UFIVSScriptNode
+
+	void SetOutputNum(int32 OutputNum);
+};
