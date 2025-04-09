039eef6be1db29002e1aa3cf1b609c3e1404483d
Roze <roze.doyanawa@gmail.com>
2025-04-09 17:42:07 +0200

    feat:Modular control panel; initial release

---
+++ Source/FicsItNetworks/Public/Components/FINSectionablePanelAttachmentPointType.h
@@ -0,0 +1,26 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FGAttachmentPoint.h"
+#include "Runtime/CoreUObject/Public/Templates/SubclassOf.h"
+
+#include "FINSectionablePanelAttachmentPointType.generated.h"
+
+class UFGAttachmentPointType;
+/**
+ * 
+ */
+UCLASS()
+class FICSITNETWORKS_API UFINSectionablePanelAttachmentPointType : public UFGAttachmentPointType {
+	GENERATED_BODY()
+	
+	public:
+
+	UFUNCTION(BlueprintCallable)
+	TArray< TSubclassOf< UFGAttachmentPointType > > GetAllowedAttachmentPointSnapTypes();
+	virtual bool CanAttach_Implementation(const FFGAttachmentPoint& point, const FFGAttachmentPoint& targetPoint) const override;
+};
+
+
--- Source/FicsItNetworksMisc/Public/ModuleSystem/FINModuleSystemHolo.h
+++ Source/FicsItNetworksMisc/Public/ModuleSystem/FINModuleSystemHolo.h
@@ -73,4 +73,5 @@ private:
 
 	UPROPERTY()
 	UStaticMeshComponent* CompassRose = nullptr;
+
 };
